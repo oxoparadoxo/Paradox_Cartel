e2660d8ad3703883f964403f04aa175c89157c72
paradox <50043821+oxoparadoxo@users.noreply.github.com>
2023-10-07 16:22:11 +0530

    Commit 2: Finished Logic of Matching in Scripting wise
    Change Log:
    1. Finished logic of Matching cards via Scripts and Logs.

--- Assets/Cardtel_Mafia/Scripts/GridManager.cs
+++ Assets/Cardtel_Mafia/Scripts/GridManager.cs
@@ -1,38 +1,115 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    public int _Width;
+    public int _Width,_Height,_Resolution;
+    public bool _Floor = false;
     public Dictionary<int,int> Level = new Dictionary<int,int>();
     public int randomNumber = 0;
     public int Row = 0;
     public int Column = 0;
     public List<int> GridNumbers = new List<int>();
+    public List<int> RandomNumbers = new List<int>();
     public int PairCounter = 1;
+    public int UniqueIndex = 0;
     private void Start()
     {
+        CheckValidInput();
         GenerateGrid();
         GenerateList();
+        UniqueIndex = 1;
+
     }
 
     void GenerateList()
     {
-        for (int i = 0; i < _Width*_Width; i++)
+        CheckValidInput();
+        for (int i = 0; i < _Width*_Height; i++)
         {
-            randomNumber = Random.Range(1, (_Width*_Width+1));
+            _Resolution = _Width * _Height;
+            
+            randomNumber = Random.Range(0, (_Width*_Height));
             Debug.Log(randomNumber);
-            GridNumbers.Add(randomNumber);
+            
+            while (RandomNumbers.Contains(randomNumber)) 
+            {
+                randomNumber = Random.Range(0, (_Width * _Height)); 
+            }
+            RandomNumbers.Add(randomNumber);
+            if(UniqueIndex > 0 && UniqueIndex < (((_Width*_Height)/2)+1))
+            { 
+                Level.Add(RandomNumbers[i], UniqueIndex);
+                GridNumbers.Add(UniqueIndex);
+                UniqueIndex++;
+            }
+            else
+            {
+                UniqueIndex = 1;
+                Level.Add(RandomNumbers[i], UniqueIndex);
+                GridNumbers.Add(UniqueIndex);
+                UniqueIndex++;
+            }    
         }
     }
     void GenerateGrid()
     {
-        for (int Row = 0; Row < _Width; Row++)
+        CheckValidInput();
+        for (int Row = 0; Row < _Height; Row++)
         {
             for (int Column = 0; Column < _Width; Column++)
             {
                 Debug.Log("Row : " + Row + " & Column : " + Column);
+            }
+        }
+    }
+
+    void CheckValidInput()
+    {
+        if(_Width == 0)
+        {
+            _Width = 2;
+        }
+
+        if(_Height == 0)
+        {
+            _Height = 2;
+        }
+
+        while (_Resolution % 2 != 0)
+        {
+            if (_Width % 2 == 0)
+            {
+                if (_Floor && _Height > 2)
+                {
+                    _Height--;
+                }
+                else if (_Height < 2)
+                {
+                    _Height++;
+                }
+                else
+                {
+                    _Height = 2;
+                }
+            }
+            else
+            {
+                if (_Floor && _Width > 2)
+                {
+                    _Width--;
+                }
+                else if (_Width < 2)
+                {
+                    _Width++;
+                }
+                else
+                {
+                    _Width = 2;
+                }
             }
         }
     }
