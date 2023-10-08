f7b5f52cbdc8a01f828f352bb9bc72958432c0ea
paradox <50043821+oxoparadoxo@users.noreply.github.com>
2023-10-08 07:56:29 +0530

    Commit 5: Level Logic Implemented with Score and Try
    Change Log:
    1. Level Logic Implemented
    2. Score is Incremented and Try is Incremented for each correct pair
    3. Try is Incremented for each wrong Pair
    4. 4x4 level will have Score 8 for Try 8 for no wrong selection

--- Assets/Cardtel_Mafia/Scripts/LevelManager.cs
+++ Assets/Cardtel_Mafia/Scripts/LevelManager.cs
@@ -4,15 +4,110 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    public GameObject SelectedTile;
+    public GameObject PairTile;
+    public int PairCount = 0;
+    public int Score = 0;
+    public int Try = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        PairCount = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetMouseButtonDown(0))
+        {
+            SelectRotateTile();
+        }
+
+    }
+
+    void SelectRotateTile()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if(PairCount >0 && PairCount <3)
+            {
+                GameObject go = hit.collider.gameObject;
+                if (go != null)
+                {
+                    Debug.Log(go.name);
+                    go.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+                }
+                if(PairCount == 1)
+                {
+                    SelectedTile = go;
+                    PairCount++;
+                }
+                else if(PairCount == 2)
+                {
+                    PairTile = go;
+                    PairCount++;
+                }
+                if (PairTile.gameObject != null && SelectedTile.gameObject != null)
+                {
+                    if (PairTile.name.Equals(SelectedTile.name))
+                    {
+                        CancelInvoke();
+                        Invoke("DelayCorrectPair", 1f);
+                    }
+                    else
+                    {
+                        CancelInvoke();
+                        Invoke("DelayWrongPair", 1f);
+                    }
+                }
+            }
+            else
+            {
+                PairCount = 1;
+                GameObject go = hit.collider.gameObject;
+                if (go != null)
+                {
+                    Debug.Log(go.name);
+                    go.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+                }
+                if (PairCount == 1)
+                {
+                    SelectedTile = go;
+                    PairCount++;
+                }
+            }
+            
+        }
+    }
+
+    void DelayCorrectPair()
+    {
+        if (PairTile.gameObject != null)
+        {
+            PairTile.SetActive(false);
+        }
+        if (SelectedTile.gameObject != null)
+        {
+            SelectedTile.SetActive(false);
+        }
         
+        Score++;
+        Try++;
+    }
+
+    void DelayWrongPair()
+    {
+        if(PairTile.gameObject != null)
+        {
+            PairTile.transform.rotation = Quaternion.Euler(new Vector3(180, 0, 0));
+        }
+        if(SelectedTile.gameObject != null)
+        {
+            SelectedTile.transform.rotation = Quaternion.Euler(new Vector3(180, 0, 0));
+        }
+        
+        Try++;
     }
 }
